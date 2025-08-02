ce27ebff9c235c47904d27cf008736869969a05d
lavey404 <laveyluo@gmail.com>
2025-08-02 17:29:28 +0800

    Update diy-part2-6.x.sh

--- diy-part2-6.x.sh
+++ diy-part2-6.x.sh
@@ -11,8 +11,8 @@
 # sed -i "/.*uhttpd.*/d" .config
 # sed -i '/.*\/etc\/init.d.*/d' package/network/services/uhttpd/Makefile
 # sed -i '/.*.\/files\/uhttpd.init.*/d' package/network/services/uhttpd/Makefile
-sed -i "s/:80/:81/g" package/network/services/uhttpd/files/uhttpd.config
-sed -i "s/:443/:4443/g" package/network/services/uhttpd/files/uhttpd.config
+#sed -i "s/:80/:81/g" package/network/services/uhttpd/files/uhttpd.config
+#sed -i "s/:443/:4443/g" package/network/services/uhttpd/files/uhttpd.config
 cp -a $GITHUB_WORKSPACE/configfiles/etc/* package/base-files/files/etc/
 # ls package/base-files/files/etc/
 
@@ -29,36 +29,36 @@ git clone --depth=1 https://github.com/sirpdboy/luci-app-eqosplus package/luci-a
 
 
 # 增加nsy_g68-plus
-echo -e "\\ndefine Device/nsy_g68-plus
-\$(call Device/Legacy/rk3568,\$(1))
-  DEVICE_VENDOR := NSY
-  DEVICE_MODEL := G68
-  DEVICE_DTS := rk3568/rk3568-nsy-g68-plus
-  DEVICE_PACKAGES += kmod-nvme kmod-ata-ahci-dwc kmod-thermal kmod-switch-rtl8306 kmod-switch-rtl8366-smi kmod-switch-rtl8366rb kmod-switch-rtl8366s kmod-hwmon-pwmfan kmod-r8169 kmod-switch-rtl8367b swconfig kmod-swconfig kmod-mt7916-firmware
-endef
-TARGET_DEVICES += nsy_g68-plus" >> target/linux/rockchip/image/legacy.mk
+#echo -e "\\ndefine Device/nsy_g68-plus
+#\$(call Device/Legacy/rk3568,\$(1))
+#  DEVICE_VENDOR := NSY
+#  DEVICE_MODEL := G68
+#  DEVICE_DTS := rk3568/rk3568-nsy-g68-plus
+#  DEVICE_PACKAGES += kmod-nvme kmod-ata-ahci-dwc kmod-thermal kmod-switch-rtl8306 kmod-switch-rtl8366-smi kmod-switch-rtl8366rb kmod-switch-rtl8366s kmod-hwmon-pwmfan kmod-r8169 kmod-switch-rtl8367b swconfig kmod-swconfig kmod-mt7916-firmware
+#endef
+#TARGET_DEVICES += nsy_g68-plus" >> target/linux/rockchip/image/legacy.mk
 
 
 # 增加nsy_g16-plus
-echo -e "\\ndefine Device/nsy_g16-plus
-\$(call Device/Legacy/rk3568,\$(1))
-  DEVICE_VENDOR := NSY
-  DEVICE_MODEL := G16
-  DEVICE_DTS := rk3568/rk3568-nsy-g16-plus
-  DEVICE_PACKAGES += kmod-nvme kmod-ata-ahci-dwc kmod-thermal kmod-switch-rtl8306 kmod-switch-rtl8366-smi kmod-switch-rtl8366rb kmod-switch-rtl8366s kmod-hwmon-pwmfan kmod-r8169 kmod-switch-rtl8367b swconfig kmod-swconfig kmod-mt7615-firmware
-endef
-TARGET_DEVICES += nsy_g16-plus" >> target/linux/rockchip/image/legacy.mk
+#echo -e "\\ndefine Device/nsy_g16-plus
+#\$(call Device/Legacy/rk3568,\$(1))
+#  DEVICE_VENDOR := NSY
+#  DEVICE_MODEL := G16
+#  DEVICE_DTS := rk3568/rk3568-nsy-g16-plus
+#  DEVICE_PACKAGES += kmod-nvme kmod-ata-ahci-dwc kmod-thermal kmod-switch-rtl8306 kmod-switch-rtl8366-smi kmod-switch-rtl8366rb kmod-switch-rtl8366s kmod-hwmon-pwmfan kmod-r8169 kmod-switch-rtl8367b swconfig kmod-swconfig kmod-mt7615-firmware
+#endef
+#TARGET_DEVICES += nsy_g16-plus" >> target/linux/rockchip/image/legacy.mk
 
 
 # 增加bdy_g18-pro
-echo -e "\\ndefine Device/bdy_g18-pro
-\$(call Device/Legacy/rk3568,\$(1))
-  DEVICE_VENDOR := BDY
-  DEVICE_MODEL := G18
-  DEVICE_DTS := rk3568/rk3568-bdy-g18-pro
-  DEVICE_PACKAGES += kmod-nvme kmod-ata-ahci-dwc kmod-thermal kmod-switch-rtl8306 kmod-switch-rtl8366-smi kmod-switch-rtl8366rb kmod-switch-rtl8366s kmod-hwmon-pwmfan kmod-r8169 kmod-switch-rtl8367b swconfig kmod-swconfig kmod-mt7615-firmware
-endef
-TARGET_DEVICES += bdy_g18-pro" >> target/linux/rockchip/image/legacy.mk
+#echo -e "\\ndefine Device/bdy_g18-pro
+#\$(call Device/Legacy/rk3568,\$(1))
+#  DEVICE_VENDOR := BDY
+#  DEVICE_MODEL := G18
+#  DEVICE_DTS := rk3568/rk3568-bdy-g18-pro
+#  DEVICE_PACKAGES += kmod-nvme kmod-ata-ahci-dwc kmod-thermal kmod-switch-rtl8306 kmod-switch-rtl8366-smi kmod-switch-rtl8366rb kmod-switch-rtl8366s kmod-hwmon-pwmfan kmod-r8169 kmod-switch-rtl8367b swconfig kmod-swconfig kmod-mt7615-firmware
+#endef
+#TARGET_DEVICES += bdy_g18-pro" >> target/linux/rockchip/image/legacy.mk
 
 # 增加wxy-oec-turbo-4g
 echo -e "\\ndefine Device/rk3566-wxy-oec-turbo-4g
@@ -78,25 +78,25 @@ cp -f $GITHUB_WORKSPACE/configfiles/02_network target/linux/rockchip/armv8/base-
 
 
 # 加入nsy_g68-plus初始化网络配置脚本
-cp -f $GITHUB_WORKSPACE/configfiles/swconfig_install package/base-files/files/etc/init.d/swconfig_install
-chmod 755 package/base-files/files/etc/init.d/swconfig_install
+#cp -f $GITHUB_WORKSPACE/configfiles/swconfig_install package/base-files/files/etc/init.d/swconfig_install
+#chmod 755 package/base-files/files/etc/init.d/swconfig_install
 
 
 # 集成 nsy_g68-plus WiFi驱动
-mkdir -p package/base-files/files/lib/firmware/mediatek
-cp -f $GITHUB_WORKSPACE/configfiles/WirelessDriver/mt7916_eeprom.bin package/base-files/files/lib/firmware/mediatek/mt7916_eeprom.bin
+#mkdir -p package/base-files/files/lib/firmware/mediatek
+#cp -f $GITHUB_WORKSPACE/configfiles/WirelessDriver/mt7916_eeprom.bin package/base-files/files/lib/firmware/mediatek/mt7916_eeprom.bin
 
 
 # rtl8367b驱动压缩包，暂时使用这样替换
-wget https://github.com/xiaomeng9597/files/releases/download/files/rtl8367b-fix-gmac.tar.gz
-tar -xvf rtl8367b-fix-gmac.tar.gz
+#wget https://github.com/xiaomeng9597/files/releases/download/files/rtl8367b-fix-gmac.tar.gz
+#tar -xvf rtl8367b-fix-gmac.tar.gz
 
 
 # 复制dts设备树文件到指定目录下
-cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3588-orangepi-5-plus.dts target/linux/rockchip/dts/rk3588/rk3588-orangepi-5-plus.dts
-cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3568-nsy-g68-plus.dts target/linux/rockchip/dts/rk3568/rk3568-nsy-g68-plus.dts
-cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3568-nsy-g16-plus.dts target/linux/rockchip/dts/rk3568/rk3568-nsy-g16-plus.dts
-cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3568-bdy-g18-pro.dts target/linux/rockchip/dts/rk3568/rk3568-bdy-g18-pro.dts
+#cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3588-orangepi-5-plus.dts target/linux/rockchip/dts/rk3588/rk3588-orangepi-5-plus.dts
+#cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3568-nsy-g68-plus.dts target/linux/rockchip/dts/rk3568/rk3568-nsy-g68-plus.dts
+#cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3568-nsy-g16-plus.dts target/linux/rockchip/dts/rk3568/rk3568-nsy-g16-plus.dts
+#cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3568-bdy-g18-pro.dts target/linux/rockchip/dts/rk3568/rk3568-bdy-g18-pro.dts
 cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3566.dtsi target/linux/rockchip/dts/rk3568/rk3566.dtsi
 cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3566-base.dtsi target/linux/rockchip/dts/rk3568/rk3566-base.dtsi
 cp -f $GITHUB_WORKSPACE/configfiles/dts/rk356x-base.dtsi target/linux/rockchip/dts/rk3568/rk356x-base.dtsi
